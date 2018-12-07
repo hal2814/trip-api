837396a1f603b01dc68291e590339bb77c747d9e
Nate McGregor <natecmcgregor@gmail.com>
2018-12-07 15:39:06 -0800

    create initial stat spec for validation and reference to trip

--- spec/models/stat_spec.rb
+++ spec/models/stat_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Stat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:trip) }
+  it { should validate_presence_of(:max_speed) }
+  it { should validate_presence_of(:distance) }
 end
