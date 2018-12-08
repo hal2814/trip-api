d21fa39a2250ec44ff95aef0843e69f7e407ad36
Nate McGregor <natecmcgregor@gmail.com>
2018-12-07 16:30:15 -0800

    create helper files for requests and parsing json to hash

---
+++ spec/requests/trips_spec.rb
@@ -0,0 +1,99 @@
+require 'rails_helper'
+
+RSpec.describe 'Trips API', type: :request do
+  let!(:trips) { create_list(:trip, 10) }
+  let(:trip_id) { trips.first.id }
+
+  describe 'GET /trips' do
+    before { get '/trips' }
+
+    it 'returns trips' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /trips/:id' do
+    before { get "/trips/#{trip_id}" }
+
+    context 'when the record exists' do
+      it 'returns the trip' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(trip_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:trip_id) { 100 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Todo/)
+      end
+    end
+  end
+
+  describe 'POST /trips' do
+    let(:valid_attributes) { { trip_id: '4593456', start: '145', stop: '345', status: "completed", vehicleId: '1092834576', deviceId: '98273' } }
+
+    context 'when the request is valid' do
+      before { post '/trips', params: valid_attributes }
+
+      it 'creates a trip' do
+        expect(json['trip_id']).to eq('4593456')
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when the request is invalid' do
+      before { post '/trips', params: { trip_id: 'invalid_request' } }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body)
+          .to match(/Validation failed: start can't be blank/)
+      end
+    end
+  end
+
+  describe 'PUT /trips/:id' do
+    let(:valid_attributes) { { trip_id: '9827364' } }
+
+    context 'when the record exists' do
+      before { put "/trips/#{trip_id}", params: valid_attributes }
+
+      it 'updates the record' do
+        expect(response.body).to be_empty
+      end
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
+    end
+  end
+
+  describe 'DELETE /trips/:id' do
+    before { delete "/trips/#{trip_id}" }
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+  end
+end
