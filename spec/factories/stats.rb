8d320aef14ade9f161e07249604e02fe77242e16
Nate McGregor <natecmcgregor@gmail.com>
2018-12-07 16:32:11 -0800

    create trips controller, and draw routes

--- spec/factories/stats.rb
+++ spec/factories/stats.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :stat do
-    max_speed { "MyString" }
-    distance { "MyString" }
+    max_speed { Faker::Number.between(50,85) }
+    distance { Faker::Number.number(4) }
     trip { nil }
   end
 end
--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :trip do
-    trip_id { "trip id example" }
-    start { "start example" }
-    stop { "stop example" }
-    status { "status example" }
-    vehicleId { "vehicleId example" }
-    deviceId { "deviceId example" }
+    trip_id { Faker::Number.number(7) }
+    start { Faker::Number.number(3) }
+    stop { Faker::Number.number(3) }
+    status { "completed" }
+    vehicleId { Faker::Number.number(10) }
+    deviceId { Faker::Number.number(5) }
   end
 end
