5355f321a0767763ae9fede15eb5a2339fe87c4d
Nate McGregor <natecmcgregor@gmail.com>
2018-12-08 23:00:19 -0800

    install and setup devise authentication system

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,6 @@ Bundler.require(*Rails.groups)
 
 module TripApi
   class Application < Rails::Application
-    config.api_only = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   root to: "trips#index"
 
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
   resources :trips do
     resources :stats
     collection do
